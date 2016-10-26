018d2df7cdb327a13596ff08ff118b0a1ce6058a
ArifeOzdemir <ozdemir.ariffe@gmail.com>
2016-10-26 18:32:18 +0300

    .

--- KariyerPortali.Admin/Controllers/MediaController.cs
+++ KariyerPortali.Admin/Controllers/MediaController.cs
@@ -13,5 +13,9 @@ namespace KariyerPortali.Admin.Controllers
         {
             return View();
         }
+        public ActionResult Create()
+        {
+            return View();
+        }
     }
 }
