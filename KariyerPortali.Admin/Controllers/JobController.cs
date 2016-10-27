c3d7ca68461522155548df67e43ae53800494209
ArifeOzdemir <ozdemir.ariffe@gmail.com>
2016-10-27 16:33:35 +0300

    .

--- KariyerPortali.Admin/Controllers/JobController.cs
+++ KariyerPortali.Admin/Controllers/JobController.cs
@@ -17,5 +17,9 @@ namespace KariyerPortali.Admin.Controllers
         {
             return View();
         }
+        public ActionResult Liste()
+        {
+            return View();
+        }
     }
 }
