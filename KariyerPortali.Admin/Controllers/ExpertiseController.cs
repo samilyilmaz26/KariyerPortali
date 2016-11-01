5cf03aa94b05312a9508d9ab754d4766aef75614
ezgiustebay <ebaran23>
2016-11-01 14:40:45 +0200

    expertisecontroller

---
+++ KariyerPortali.Admin/Controllers/ExpertiseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KariyerPortali.Admin.Controllers
+{
+    public class ExpertiseController : Controller
+    {
+        // GET: Expertise
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
