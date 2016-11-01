2e15e73a9b19a6bbef68630995826ccb01342712
betulakgul <betul.akgul44@gmail.com>
2016-11-01 15:07:57 +0200

    js

--- KariyerPortali.Admin/Scripts/pages/Country-Index.js
+++ KariyerPortali.Admin/Scripts/pages/Country-Index.js
@@ -1,1 +1,85 @@
-﻿
+﻿var initTable1 = function () {
+
+    var table = $('#allCountryTable');
+    // begin first table
+    table.dataTable({
+
+        // Internationalisation. For more info refer to http://datatables.net/manual/i18n
+        "language": {
+            "aria": {
+                "sortAscending": ": activate to sort column ascending",
+                "sortDescending": ": activate to sort column descending"
+            },
+            "emptyTable": "Tabloda uygun veri yok",
+            "info": "_TOTAL_ kaydın _START_ ile _END_ arası gösteriliyor",
+            "infoEmpty": "Kayıt bulunamadı",
+            "infoFiltered": "(filtered1 from _MAX_ total records)",
+            "lengthMenu": "Sayfa başına _MENU_ kayıt göster",
+            "search": "Ara:",
+            "zeroRecords": "Arama kriterinizle eşleşen kayıt yok",
+            "paginate": {
+                "previous": "Önceki",
+                "next": "Sonraki",
+                "last": "Son",
+                "first": "İlk"
+            }
+        },
+
+        // Or you can use remote translation file
+        //"language": {
+        //   url: '//cdn.datatables.net/plug-ins/3cfcc339e89/i18n/Portuguese.json'
+        //},
+
+        // Uncomment below line("dom" parameter) to fix the dropdown overflow issue in the datatable cells. The default datatable layout
+        // setup uses scrollable div(table-scrollable) with overflow:auto to enable vertical scroll(see: assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js). 
+        // So when dropdowns used the scrollable div should be removed. 
+        //"dom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r>t<'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
+
+        "bStateSave": true, // save datatable state(pagination, sort, etc) in cookie.
+
+        "lengthMenu": [
+            [5, 15, 20, -1],
+            [5, 15, 20, "All"] // change per page values here
+        ],
+        // set the initial value
+        "pageLength": 5,
+        "pagingType": "bootstrap_full_number",
+        "columnDefs": [
+            {  // set default column settings
+                'orderable': false,
+                'targets': [0]
+            },
+            {
+                "searchable": false,
+                "targets": [0]
+            },
+            {
+                "className": "dt-right",
+                //"targets": [2]
+            }
+        ],
+        "order": [
+            [1, "asc"]
+        ] // set first column as a default sort by asc
+    });
+
+    var tableWrapper = jQuery('#allCountryTable_wrapper');
+
+    table.find('.group-checkable').change(function () {
+        var set = jQuery(this).attr("data-set");
+        var checked = jQuery(this).is(":checked");
+        jQuery(set).each(function () {
+            if (checked) {
+                $(this).prop("checked", true);
+                $(this).parents('tr').addClass("active");
+            } else {
+                $(this).prop("checked", false);
+                $(this).parents('tr').removeClass("active");
+            }
+        });
+    });
+
+    table.on('change', 'tbody tr .checkboxes', function () {
+        $(this).parents('tr').toggleClass("active");
+    });
+}
