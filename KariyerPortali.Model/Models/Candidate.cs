f86195d9b8a624a24bd80bb69dfecda53e1804c7
Bora <boraa.karaca@gmail.com>
2016-11-01 16:32:21 +0200

    candidate bitti

--- KariyerPortali.Model/Models/Candidate.cs
+++ KariyerPortali.Model/Models/Candidate.cs
@@ -6,9 +6,22 @@ using System.Threading.Tasks;
 
 namespace KariyerPortali.Model
 {
-    class Candidate
+    public class Candidate
     {
         public int CandidateId { get; set; }
         public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string image { get; set; }
+        public DateTime CreateDate{ get; set; }
+        public bool State { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+
+
     }
 }
