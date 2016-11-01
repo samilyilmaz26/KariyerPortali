f5b0bea5def62359a45b981b8aff41289e6d4bd1
Esraylmz <yilmaz-.-esra@hotmail.com>
2016-11-01 14:53:36 +0200

    department

--- KariyerPortali.Admin/Scripts/pages/department-index.js
+++ KariyerPortali.Admin/Scripts/pages/department-index.js
@@ -10,18 +10,18 @@
                 "sortAscending": ": activate to sort column ascending",
                 "sortDescending": ": activate to sort column descending"
             },
-            "emptyTable": "No data available in table",
-            "info": "Showing _START_ to _END_ of _TOTAL_ records",
-            "infoEmpty": "No records found",
-            "infoFiltered": "(filtered1 from _MAX_ total records)",
+            "emptyTable": "Böyke bir kayıt bulunamamaktadır",
+            "info": "Gösterilen _START_ ile _END_ toplam _TOTAL_ kayıt",
+            "infoEmpty": "Kayıt bulunamadı",
+            "infoFiltered": "(Filitrenilen toplam _MAX_ kayıt)",
             "lengthMenu": "Göster _MENU_",
-            "search": "Ara:",
-            "zeroRecords": "No matching records found",
+            "search": "Arama:",
+            "zeroRecords": "Eşleşen kayıt bulunmamaktadır",
             "paginate": {
-                "previous": "Prev",
-                "next": "Next",
-                "last": "Last",
-                "first": "First"
+                "previous": "Önceki",
+                "next": "Sonraki",
+                "last": "Son",
+                "first": "İlk"
             }
         },
 
@@ -63,7 +63,7 @@
         ] // set first column as a default sort by asc
     });
 
-    var tableWrapper = jQuery('#allApplicationsTable_wrapper');
+    var tableWrapper = jQuery('#allDepartmentTable_wrapper');
 
     table.find('.group-checkable').change(function () {
         var set = jQuery(this).attr("data-set");
