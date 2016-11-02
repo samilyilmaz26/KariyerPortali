b95920e3d656e4702e7da0d66a0289c6bf46f1b0
CEVDET29 <cevdetbas29@gmail.com>
2016-11-02 17:58:09 +0200

    ss

--- KariyerPortali.Data/Configuration/EmployerConfiguration.cs
+++ KariyerPortali.Data/Configuration/EmployerConfiguration.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using KariyerPortali.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KariyerPortali.Data.Configuration
 {
-    class EmployerComfiguration
+    public class EmployerComfiguration:EntityTypeConfiguration<Employer>
     {
+        public EmployerComfiguration()
+        {
+            ToTable("Employers");
+            HasKey<int>(c => c.EmployerId);
+            Property(c=>c.EmployerName).
+        }
     }
 }
