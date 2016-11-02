1d9705481899a14bae94e87ed5d842e1ecaa4890
AysenurGunes <gunesaysenur94@gmail.com>
2016-11-02 17:49:53 +0200

    languageConfiguration

--- KariyerPortali.Data/Configuration/LanguageConfiguration.cs
+++ KariyerPortali.Data/Configuration/LanguageConfiguration.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using KariyerPortali.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KariyerPortali.Data.Configuration
 {
-    class LanguageConfiguration
+   public class LanguageConfiguration:EntityTypeConfiguration<Language>
     {
+         public LanguageConfiguration()
+        {
+            /* Fluent-API */
+            ToTable("Languages");
+            HasKey<int>(c=>c.LanguageId);
+            Property(c => c.LanguageName).IsRequired().HasMaxLength(100);
+        }
     }
 }
