a9662e18aa7d04fc3538b213777a01bfe771e5c2
ezgiustebay <ebaran23>
2016-11-02 17:39:13 +0200

    candidate

--- KariyerPortali.Admin/ViewModels/CandidateViewModel.cs
+++ KariyerPortali.Admin/ViewModels/CandidateViewModel.cs
@@ -7,5 +7,17 @@ namespace KariyerPortali.Admin.ViewModels
 {
     public class CandidateViewModel
     {
+        public int CandidateId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Photo { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool State { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
