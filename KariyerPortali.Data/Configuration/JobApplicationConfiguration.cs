17e0fc5981922248b10b6032ad6c7be0f0c6e0e6
AysenurGunes <gunesaysenur94@gmail.com>
2016-11-02 18:07:52 +0200

    jobApplicationConfiguration

--- KariyerPortali.Data/Configuration/JobApplicationConfiguration.cs
+++ KariyerPortali.Data/Configuration/JobApplicationConfiguration.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using KariyerPortali.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KariyerPortali.Data.Configuration
 {
-    class JobApplicationConfiguration
+   public class JobApplicationConfiguration:EntityTypeConfiguration<JobApplication>
     {
+       public JobApplicationConfiguration()
+        {
+            /* Fluent-API */
+            ToTable("JobApplications");
+            HasKey<int>(c=>c.ApplicationId);
+            Property(c => c.CoverLetter).HasMaxLength(500);
+           
+        }
     }
 }
