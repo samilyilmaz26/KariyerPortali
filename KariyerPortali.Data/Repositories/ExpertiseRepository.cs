cb46d5c0f7d57a1924dc4c7a8de12dcdb8d7fec3
betulakgul <betul.akgul44@gmail.com>
2016-11-02 15:56:02 +0200

    r

---
+++ KariyerPortali.Data/Repositories/ExpertiseRepository.cs
@@ -0,0 +1,23 @@
+﻿using KariyerPortali.Data.Infrastructure;
+using KariyerPortali.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KariyerPortali.Data.Repositories
+{
+    class ExpertiseRepository
+    {
+    }
+    public class ExpertiseRepository : RepositoryBase<Expertise>, IExpertiseRepository
+    {
+        public ExpertiseRepository(IDbFactory dbFactory)
+            : base(dbFactory) { }
+    }
+    public interface IExpertiseRepository : IRepository<Expertise>
+    {
+
+    }
+}
