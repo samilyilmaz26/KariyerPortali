738f5aab7227437ce3aa2983ecb868dd471c0baf
yetkinkaymak <yetkinkaymak@gmail.com>
2016-11-02 18:41:27 +0200

    job

---
+++ KariyerPortali.Service/JobService.cs
@@ -0,0 +1,59 @@
+﻿using KariyerPortali.Data.Infrastructure;
+using KariyerPortali.Data.Repositories;
+using KariyerPortali.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KariyerPortali.Service
+{
+    public interface IJobService
+    {
+        IEnumerable<Job> GetJobs();
+        Job GetJob(int id);
+        void CreateJob(Job job);
+        void UpdateJob(Job job);
+        void DeleteJob(Job job);
+        void SaveJob();
+    }
+    public class JobService : IJobService
+    {
+        private readonly IJobRepository jobRepository;
+        private readonly IUnitOfWork unitOfWork;
+        public JobService(IJobRepository jobRepository, IUnitOfWork unitOfWork)
+        {
+            this.jobRepository = jobRepository;
+            this.unitOfWork = unitOfWork;
+        }
+        #region IJobService Members
+        public IEnumerable<Job> GetJobs()
+        {
+            var jobs = jobRepository.GetAll();
+            return jobs;
+        }
+        public Job GetJob(int id)
+        {
+            var job = jobRepository.GetById(id);
+            return job;
+        }
+        public void CreateJob(Job job)
+        {
+            jobRepository.Add(job);
+        }
+        public void UpdateJob(Job job)
+        {
+            jobRepository.Update(job);
+        }
+        public void DeleteJob(Job job)
+        {
+            jobRepository.Delete(job);
+        }
+        public void SaveJob()
+        {
+            unitOfWork.Commit();
+        }
+        #endregion
+    }
+}
