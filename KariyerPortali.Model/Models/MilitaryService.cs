d6dd79db46c3c58f42c4a965806e0a0b04f20601
Bora <boraa.karaca@gmail.com>
2016-11-02 16:47:44 +0200

    askerlik eklendi

---
+++ KariyerPortali.Model/Models/MilitaryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KariyerPortali.Model
+{
+    public enum MilitaryService
+    {
+        Done=1,
+        Postpone=2,
+        Exempt=3
+    }
+}
