89ac7a1bd3c84f02559c79266006a59428439a7a
Bora <boraa.karaca@gmail.com>
2016-11-02 17:38:46 +0200

    skill info eklendi

--- KariyerPortali.Model/Models/ExamInfo.cs
+++ KariyerPortali.Model/Models/ExamInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace KariyerPortali.Model
 {
     public class ExamInfo
     {
+        [ForeignKey("Exam")]
+        public int ExamId { get; set; }
+        public Exam Exam { get; set; }
+        public float Point { get; set; }
+
     }
 }
