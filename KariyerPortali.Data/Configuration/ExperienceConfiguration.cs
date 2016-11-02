f96a86cb9662643b6386acc19d4c1c7e206b1803
CEVDET29 <cevdetbas29@gmail.com>
2016-11-02 18:15:25 +0200

    exp conf

---
+++ KariyerPortali.Data/Configuration/ExperienceConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using KariyerPortali.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KariyerPortali.Data.Configuration
+{
+    public class ExperienceConfiguration:EntityTypeConfiguration<Experience>
+    {
+        public ExperienceConfiguration
+    }
+}
