f6b25f2b824dee011b9eb0fbf6af51a4e31c4f6c
CEVDET29 <cevdetbas29@gmail.com>
2016-11-02 16:31:15 +0200

    dbsetler kelnedi

--- KariyerPortali.Data/KariyerPortaliEntities.cs
+++ KariyerPortali.Data/KariyerPortaliEntities.cs
@@ -13,8 +13,24 @@ namespace KariyerPortali.Data
     {
         public KariyerPortaliEntities() : base("KariyerPortaliEntities") { }
 
+        public DbSet<Candidate> Candidates { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Candidate { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employer> Employers { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Expertise> Expertises { get; set; }
+        public DbSet<File> Files { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<JobApplication> JobApplications { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Post> Posts { get; set; }
         public DbSet<Resume> Resumes { get; set; }
+        public DbSet<Sector> Sectors { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialRight> SocialRights { get; set; }
         public DbSet<University> Universities { get; set; }
+
 
         public virtual void Commit()
         {
