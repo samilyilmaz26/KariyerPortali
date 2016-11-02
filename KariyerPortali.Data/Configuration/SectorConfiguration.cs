54931a4d07ca7623d78c429b68dae233b95bbfde
AysenurGunes <gunesaysenur94@gmail.com>
2016-11-02 17:32:30 +0200

    sector configuration

--- KariyerPortali.Data/Configuration/SectorConfiguration.cs
+++ KariyerPortali.Data/Configuration/SectorConfiguration.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using KariyerPortali.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KariyerPortali.Data.Configuration
 {
-    class SectorConfiguration
+  public  class SectorConfiguration:EntityTypeConfiguration<Sector>
     {
+            public SectorConfiguration()
+        {
+            /* Fluent-API */
+            ToTable("Sectors");
+            HasKey<int>(c=>c.SectorId);
+            Property(c => c.SectorName).IsRequired().HasMaxLength(200);
+        }
     }
 }
