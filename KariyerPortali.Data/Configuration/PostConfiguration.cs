87281cf216332b8e6d0a5d9e22ba7cdc03fe5147
AysenurGunes <gunesaysenur94@gmail.com>
2016-11-02 17:41:51 +0200

    post Configuration

--- KariyerPortali.Data/Configuration/PostConfiguration.cs
+++ KariyerPortali.Data/Configuration/PostConfiguration.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using KariyerPortali.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KariyerPortali.Data.Configuration
 {
-    class PostConfiguration
+   public class PostConfiguration:EntityTypeConfiguration<Post>
     {
+       public PostConfiguration()
+        {
+            /* Fluent-API */
+            ToTable("Posts");
+            HasKey<int>(c=>c.PostId);
+            Property(c => c.Title).IsRequired().HasMaxLength(100);
+            Property(c => c.Body).IsRequired();
+        }
     }
 }
