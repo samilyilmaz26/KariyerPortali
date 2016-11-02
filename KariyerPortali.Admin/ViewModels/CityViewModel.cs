3792b2cb55c9e041ef76d2fd301431fe17d22281
muhammedsezgin <muhammed.sezgin88@gmail.com>
2016-11-02 17:00:19 +0200

    CityView

--- KariyerPortali.Admin/ViewModels/CityViewModel.cs
+++ KariyerPortali.Admin/ViewModels/CityViewModel.cs
@@ -7,8 +7,5 @@ namespace KariyerPortali.Admin.ViewModels
 {
     public class CityViewModel
     {
-        public int CityId { get; set; }
-        public string CityName { get; set; }
-        public string  CountryName { get; set; }
     }
 }
