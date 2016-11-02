db33ccbb1a8b789af3b51d4ffbfd48d28c82c0be
CEVDET29 <cevdetbas29@gmail.com>
2016-11-02 18:34:02 +0200

    file config done

--- KariyerPortali.Data/Configuration/FileConfiguration.cs
+++ KariyerPortali.Data/Configuration/FileConfiguration.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using KariyerPortali.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KariyerPortali.Data.Configuration
 {
-    class FileConfiguration
+    public class FileConfiguration:EntityTypeConfiguration<File>
     {
+        public FileConfiguration()
+        {
+            ToTable("Files");
+            HasKey<int>(c => c.FileId);
+            Property(c => c.FileName).IsRequired();
+        }
     }
 }
