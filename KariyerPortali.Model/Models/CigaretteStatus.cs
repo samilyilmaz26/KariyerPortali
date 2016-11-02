2f68dfc9b8555f8744d182f31ae76bae0e215e8e
Bora <boraa.karaca@gmail.com>
2016-11-02 18:04:07 +0200

    sigara eklendi

---
+++ KariyerPortali.Model/Models/CigaretteStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KariyerPortali.Model
+{
+    public enum CigaretteStatus
+    {
+        Yes=1,
+        No=2
+    }
+}
