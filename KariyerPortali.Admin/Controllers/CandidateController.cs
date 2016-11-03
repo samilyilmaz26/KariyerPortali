2689212757cac773d855f81bd7b91e830cccdde6
ezgiustebay <ebaran23>
2016-11-03 17:45:52 +0200

    candidateajax

--- KariyerPortali.Admin/Controllers/CandidateController.cs
+++ KariyerPortali.Admin/Controllers/CandidateController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using KariyerPortali.Admin.Models;
 using KariyerPortali.Admin.ViewModels;
 using KariyerPortali.Model;
 using KariyerPortali.Service;
@@ -34,6 +35,63 @@ namespace KariyerPortali.Admin.Controllers
 
         }
 
+        //public ActionResult AjaxHandler(jQueryDataTableParamModel param)
+        //{
+
+        //    string sSearch = "";
+        //    if (param.sSearch != null) sSearch = param.sSearch;
+        //    var allCandidates = new CandidateService().GetAllCandidates(sSearch);
+        //    IEnumerable<Candidate> filteredCandidates = allCandidates;
+
+        //    var sortColumnIndex = Convert.ToInt32(Request["iSortCol_0"]);
+
+
+        //    var sortDirection = Request["sSortDir_0"]; // asc or desc
+        //    if (sortDirection == "asc")
+        //    {
+        //        switch (sortColumnIndex)
+        //        {
+        //            case 0:
+        //                filteredCandidates = filteredCandidates.OrderBy(c => c.CandidateId);
+        //                break;
+        //            case 1:
+        //                filteredCandidates = filteredCandidates.OrderBy(c => c.UserName);
+        //                break;
+
+        //            default:
+        //                filteredCandidates = filteredCandidates.OrderBy(c => c.CandidateId);
+        //                break;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        switch (sortColumnIndex)
+        //        {
+        //            case 0:
+        //                filteredCandidates = filteredCandidates.OrderByDescending(c => c.CandidateId);
+        //                break;
+        //            case 1:
+        //                filteredCandidates = filteredCandidates.OrderByDescending(c => c.UserName);
+        //                break;
+
+        //            default:
+        //                filteredCandidates = filteredCandidates.OrderByDescending(c => c.CandidateId);
+        //                break;
+        //        }
+        //    }
+
+        //    var displayedCVs = filteredCandidates.Skip(param.iDisplayStart).Take(param.iDisplayLength);
+        //    var result = from c in displayedCVs
+        //                 select new[] { c.UserName ?? string.Empty, c.FirstName + " " + c.LastName, c.Phone.ToString(), c.State.ToString(), c.Email.ToString(), c.CreateDate.ToShortDateString() };
+        //    return Json(new
+        //    {
+        //        sEcho = param.sEcho,
+        //        iTotalRecords = allCandidates.Count(),
+        //        iTotalDisplayRecords = filteredCandidates.Count(),
+        //        aaData = result
+        //    },
+        //        JsonRequestBehavior.AllowGet);
+        //}
         public ActionResult Edit()
         {
             return View();
