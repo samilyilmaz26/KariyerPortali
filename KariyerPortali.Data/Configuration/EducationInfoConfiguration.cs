c4a36bc44de5f9f005c30d4e18a6424f909a38b3
AysenurGunes <gunesaysenur94@gmail.com>
2016-11-03 17:51:42 +0200

    .

--- KariyerPortali.Data/Configuration/EducationInfoConfiguration.cs
+++ KariyerPortali.Data/Configuration/EducationInfoConfiguration.cs
@@ -19,23 +19,4 @@ namespace KariyerPortali.Data.Configuration
         }
     }
 }
-using KariyerPortali.Model;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity.ModelConfiguration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace KariyerPortali.Data.Configuration
-{
-    public class EducationInfoConfiguration:EntityTypeConfiguration<EducationInfo>
-    {
-        public EducationInfoConfiguration()
-        {
-            ToTable("EducationInfos");
-            HasKey<int>(c => c.EducationInfoId);
-            HasOptional<Department>(c => c.Department).WithMany(c => c.EducationInfos).HasForeignKey(c => c.DepartmentId).WillCascadeOnDelete(false);
-        }
-    }
-}
