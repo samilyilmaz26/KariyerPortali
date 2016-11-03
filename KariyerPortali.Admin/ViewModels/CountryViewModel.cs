5cdedc497c798c5534087888969db286a7edeb3a
betulakgul <betul.akgul44@gmail.com>
2016-11-03 14:38:38 +0200

    view

---
+++ KariyerPortali.Admin/ViewModels/CountryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KariyerPortali.Admin.ViewModels
+{
+    public class CountryViewModel
+    {
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }
+     //   public ICollection<City> Cities { get; set; }
+    }
+}
