52800d966bc8a88d1bec62f9422e37e50f46cc99
Bora <boraa.karaca@gmail.com>
2016-11-03 14:42:08 +0200

    reference eklendi

---
+++ KariyerPortali.Model/Models/ReferenceType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KariyerPortali.Model
+{
+    public enum ReferenceType
+    {
+        BusinessManager=1,
+        BusinessFriend=2,
+
+        Academic=3,
+        Family=4,
+        PublicEmployee=5,
+        SocialActivity=6
+
+
+
+    }
+}
