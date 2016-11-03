9813e52e9d8c707c46fb05278b7caa59aaf34a62
betulakgul <betul.akgul44@gmail.com>
2016-11-03 16:22:24 +0200

    repo

---
+++ KariyerPortali.Data/Repositories/HighSchoolDepartmentRepository.cs
@@ -0,0 +1,23 @@
+﻿using KariyerPortali.Data.Infrastructure;
+using KariyerPortali.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KariyerPortali.Data.Repositories
+{
+    class HighSchoolDepartmentRepository
+    {
+    }
+    public class HighSchoolDepartmentRepository : RepositoryBase<HighSchoolDepartment>, IHighSchoolDepartmentRepository
+    {
+        public HighSchoolDepartmentRepository(IDbFactory dbFactory)
+            : base(dbFactory) { }
+    }
+    public interface IHighSchoolDepartmentRepository : IRepository<HighSchoolDepartment>
+    {
+
+    }
+}
