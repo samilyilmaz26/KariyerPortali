a6a1dc87422b34cc5e621bd45c698225d42ea485
CEVDET29 <cevdetbas29@gmail.com>
2016-11-03 17:49:31 +0200

    qq

--- KariyerPortali.Data/Configuration/SkillInfoConfiguration.cs
+++ KariyerPortali.Data/Configuration/SkillInfoConfiguration.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using KariyerPortali.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KariyerPortali.Data.Configuration
 {
-    class SkillInfoConfiguration
+    public class SkillInfoConfiguration:EntityTypeConfiguration<SkillInfo>
     {
+        public SkillInfoConfiguration()
+        {
+            ToTable("SkillInfos");
+            HasKey<int>(c => c.SkillInfoId);
+            HasOptional<Skill>(c => c.Skill).WithMany(c => c.SkillInfos).HasForeignKey(c => c.SkillInfoId).WillCascadeOnDelete(false);
+        }
     }
 }
