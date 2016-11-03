da104c891d556e7f8a59bc0622abc0b45b732e8f
AysenurGunes <gunesaysenur94@gmail.com>
2016-11-03 15:42:20 +0200

    cascadedelete

--- KariyerPortali.Data/Configuration/JobApplicationConfiguration.cs
+++ KariyerPortali.Data/Configuration/JobApplicationConfiguration.cs
@@ -16,7 +16,7 @@ namespace KariyerPortali.Data.Configuration
             ToTable("JobApplications");
             HasKey<int>(c=>c.ApplicationId);
             Property(c => c.CoverLetter).HasMaxLength(500);
-
+            HasOptional<Employer>(c => c.Employer).WithMany().WillCascadeOnDelete(false);
            
         }
     }
