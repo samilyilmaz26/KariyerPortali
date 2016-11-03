7e1db2e7e669e00741474985ae4d4088a9f64084
yetkinkaymak <yetkinkaymak@gmail.com>
2016-11-03 14:45:25 +0200

    lldsfsdfdsINFO

---
+++ KariyerPortali.Service/LanguageInfoService.cs
@@ -0,0 +1,60 @@
+﻿using KariyerPortali.Data.Infrastructure;
+using KariyerPortali.Data.Repositories;
+using KariyerPortali.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace KariyerPortali.Service
+{
+    public interface ILanguageInfoService
+    {
+        IEnumerable<LanguageInfo> GetLanguageInfos();
+        LanguageInfo GetLanguageInfo(int id);
+        void CreateLanguageInfo(LanguageInfo languageInfo);
+        void UpdateLanguageInfo(LanguageInfo languageInfo);
+        void DeleteLanguageInfo(LanguageInfo languageInfo);
+        void SaveLanguageInfo();
+    }
+    public class LanguageInfoService : ILanguageInfoService
+    {
+        private readonly ILanguageInfoRepository languageInfoRepository;
+        private readonly IUnitOfWork unitOfWork;
+        public LanguageInfoService(ILanguageInfoRepository languageInfoRepository, IUnitOfWork unitOfWork)
+        {
+            this.languageInfoRepository = languageInfoRepository;
+            this.unitOfWork = unitOfWork;
+        }
+        #region ILanguageInfoService Members
+        public IEnumerable<LanguageInfo> GetLanguageInfos()
+        {
+            var languageInfos = languageInfoRepository.GetAll();
+            return languageInfos;
+        }
+        public LanguageInfo GetLanguageInfo(int id)
+        {
+            var languageInfo=languageInfoRepository.GetById(id);
+            return languageInfo;
+        }
+        public void CreateLanguageInfo(LanguageInfo languageInfo)
+        {
+            languageInfoRepository.Add(languageInfo);
+        }
+        public void UpdateLanguageInfo(LanguageInfo languageInfo)
+        {
+            languageInfoRepository.Update(languageInfo);
+        }
+        public void DeleteLanguageInfo(LanguageInfo languageInfo)
+        {
+            languageInfoRepository.Delete(languageInfo);
+        }
+        public void SaveLanguageInfo()
+        {
+            unitOfWork.Commit();
+        }
+        #endregion
+    }
+}
