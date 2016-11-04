659ba22c8fb44ba4673898d61808329d3c784c12
muhammedsezgin <muhammed.sezgin88@gmail.com>
2016-11-04 15:35:22 +0200

    city index

--- KariyerPortali.Admin/Scripts/pages/city-index.js
+++ KariyerPortali.Admin/Scripts/pages/city-index.js
@@ -36,7 +36,7 @@
         // setup uses scrollable div(table-scrollable) with overflow:auto to enable vertical scroll(see: assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js). 
         // So when dropdowns used the scrollable div should be removed. 
         //"dom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r>t<'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
-
+        "sAjaxSource": "/City/AjaxHandler",
         "bStateSave": true, // save datatable state(pagination, sort, etc) in cookie.
 
         "lengthMenu": [
