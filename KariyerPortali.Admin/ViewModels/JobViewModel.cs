8aeaeec92c36ac51a62ebd79db04d73536953761
ecraany <eraycanbikmaz@gmail.com>
2016-11-04 14:55:53 +0200

    update

--- KariyerPortali.Admin/ViewModels/JobViewModel.cs
+++ KariyerPortali.Admin/ViewModels/JobViewModel.cs
@@ -12,10 +12,20 @@ namespace KariyerPortali.Admin.ViewModels
 
         public string Description { get; set; }
 
-        public string City { get; set; }
+
+        public int EmployerId { get; set; }
+        public Employer Employer { get; set; }
 
         public JobType JobType { get; set; }
 
+        public int ExperienceId { get; set; }
+        public Experience Experience { get; set; }
+
+        public string Responsibilities { get; set; }
+        public string Qualifications { get; set; }
         public DateTime Createdate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public string UpdatedBy { get; set; }
     }
 }
