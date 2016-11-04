cd8dfe2cbf95c382f38f9b66eee4fb534b8aa5f2
CEVDET29 <cevdetbas29@gmail.com>
2016-11-04 15:52:28 +0200

    a

--- KariyerPortali.Data/KariyerPortaliEntities.cs
+++ KariyerPortali.Data/KariyerPortaliEntities.cs
@@ -13,33 +13,33 @@ namespace KariyerPortali.Data
     {
         public KariyerPortaliEntities() : base("KariyerPortaliEntities") { }
 
-        //public DbSet<Candidate> Candidates { get; set; }
+        public DbSet<Candidate> Candidates { get; set; }
         public DbSet<City> Cities { get; set; }
         public DbSet<Country> Countries { get; set; }
-        //public DbSet<Course> Courses { get; set; }
-        //public DbSet<Department> Departments { get; set; }
-        //public DbSet<EducationInfo> EducationInfos { get; set; }
-        //public DbSet<Employer> Employers { get; set; }
-        //public DbSet<Exam> Exams { get; set; }
-        //public DbSet<ExamInfo> ExamInfos { get; set; }
-        //public DbSet<Experience> Experiences { get; set; }
-        //public DbSet<Expertise> Expertises { get; set; }
-        //public DbSet<File> Files { get; set; }
-        //public DbSet<HighSchoolDepartment> HighSchollDepartments { get; set; }
-        //public DbSet<HighSchoolType> HighSchollTypes { get; set; }
-        //public DbSet<Job> Jobs { get; set; }
-        //public DbSet<JobApplication> JobApplications { get; set; }
-        //public DbSet<Language> Languages { get; set; }
-        //public DbSet<LanguageInfo> LanguageInfos { get; set; }
-        //public DbSet<Post> Posts { get; set; }
-        //public DbSet<Reference> References { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<EducationInfo> EducationInfos { get; set; }
+        public DbSet<Employer> Employers { get; set; }
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<ExamInfo> ExamInfos { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Expertise> Expertises { get; set; }
+        public DbSet<File> Files { get; set; }
+        public DbSet<HighSchoolDepartment> HighSchollDepartments { get; set; }
+        public DbSet<HighSchoolType> HighSchollTypes { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<JobApplication> JobApplications { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<LanguageInfo> LanguageInfos { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Reference> References { get; set; }
         public DbSet<Resume> Resumes { get; set; }
-        //public DbSet<Sector> Sectors { get; set; }
-        //public DbSet<Setting> Settings { get; set; }
-        //public DbSet<Skill> Skills { get; set; }
-        //public DbSet<SkillInfo> SkillInfos { get; set; }
-        //public DbSet<SocialRight> SocialRights { get; set; }
-        //public DbSet<University> Universities { get; set; }
+        public DbSet<Sector> Sectors { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SkillInfo> SkillInfos { get; set; }
+        public DbSet<SocialRight> SocialRights { get; set; }
+        public DbSet<University> Universities { get; set; }
 
 
         public virtual void Commit()
