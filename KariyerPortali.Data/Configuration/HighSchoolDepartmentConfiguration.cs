d11bcd6049a9b6c604730f3e18d52eef3f6007ff
CEVDET29 <cevdetbas29@gmail.com>
2016-11-04 16:12:40 +0200

    asdasd

---
+++ KariyerPortali.Data/Configuration/HighSchoolDepartmentConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using KariyerPortali.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KariyerPortali.Data.Configuration
+{
+    public class HighSchoolDepartmentConfiguration:EntityTypeConfiguration<HighSchoolDepartment>
+    {
+        public HighSchoolDepartmentConfiguration()
+        {
+            ToTable("HighSchoolDepartments");
+            HasKey<int>(c => c.HighSchoolDepartmentId);
+            Property(c => c.DepartmentName).IsRequired();
+        }
+    }
+}
