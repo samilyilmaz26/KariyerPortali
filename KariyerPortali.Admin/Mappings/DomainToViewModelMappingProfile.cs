cc3078a783218803b681abe48b8628e224e6b702
ArifeOzdemir <ozdemir.ariffe@gmail.com>
2016-11-04 15:09:00 +0200

    .

--- KariyerPortali.Admin/Mappings/DomainToViewModelMappingProfile.cs
+++ KariyerPortali.Admin/Mappings/DomainToViewModelMappingProfile.cs
@@ -23,6 +23,8 @@ namespace KariyerPortali.Admin.Mappings
             Mapper.CreateMap<City, CityViewModel>();
             Mapper.CreateMap<Candidate, CandidateViewModel>();
             Mapper.CreateMap<Job, JobViewModel>();
+            Mapper.CreateMap<Language, LanguageViewModel>();
+
             Mapper.CreateMap<University, UniversityViewModel>();
 #pragma warning restore CS0618 // Type or member is obsolete
         }
