30202ef77ccb375e3ae8288008b2885d9165d3e7
yetkinkaymak <yetkinkaymak@gmail.com>
2016-11-07 15:57:12 +0200

    sdfgdsf

--- KariyerPortali.Admin/Scripts/pages/university-index.js
+++ KariyerPortali.Admin/Scripts/pages/university-index.js
@@ -52,7 +52,9 @@
                 "searchable": false,
                 "targets": [0],
                 'render':function(data,type,row){
-                    return '<label class=mt-checkbox mt-checkbox-single mt-checkbox-outline"><input type="checkbox" class="checkboxes" value="1" /><span></span></label>';
+                    return '<div class="btn-group"><button class="btn btn-xs green dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">Eylemler<i class="fa fa-angle-down"></i></button>'
+                        + '<ul class="dropdown-menu" role="menu"><li><a href="/Employer/Edit/' + row[0] + '"><i class="icon-note"></i> Düzenle</a></li><li><a href="/Employer/Details/' + row[0] + '"><i class="icon-list"></i> Detaylar</a></li><li>'
+                        + '<a href="/Employer/Delete/' + row[0] + '"><i class="icon-ban"></i> Sil</a></li></ul></div>';
                 },
                 },
 ],
