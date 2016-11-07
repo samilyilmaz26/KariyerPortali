08dc0612e086554903b0c098a1f8b54c24582ce3
ArifeOzdemir <ozdemir.ariffe@gmail.com>
2016-11-07 17:50:22 +0200

    sdgv<df

--- KariyerPortali.Admin/Scripts/pages/language-index.js
+++ KariyerPortali.Admin/Scripts/pages/language-index.js
@@ -59,11 +59,11 @@
              {
                  'orderable': false,
                  'searchable': false,
-                 'targets': [5],
+                 'targets': [3],
                  'render': function (data, type, row) {
                      return '<div class="btn-group"><button class="btn btn-xs green dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">Eylemler<i class="fa fa-angle-down"></i></button>'
-                         + '<ul class="dropdown-menu" role="menu"><li><a href="/Employer/Edit/' + row[0] + '"><i class="icon-note"></i> Düzenle</a></li><li><a href="/Employer/Details/' + row[0] + '"><i class="icon-list"></i> Detaylar</a></li><li>'
-                         + '<a href="/Employer/Delete/' + row[0] + '"><i class="icon-ban"></i> Sil</a></li></ul></div>';
+                         + '<ul class="dropdown-menu" role="menu"><li><a href="/Language/Edit/' + row[0] + '"><i class="icon-note"></i> Düzenle</a></li><li><a href="/Language/Details/' + row[0] + '"><i class="icon-list"></i> Detaylar</a></li><li>'
+                         + '<a href="/Language/Delete/' + row[0] + '"><i class="icon-ban"></i> Sil</a></li></ul></div>';
                  }
 
              }
