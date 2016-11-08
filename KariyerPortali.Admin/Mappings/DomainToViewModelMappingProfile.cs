5a7808c7bb5232337f0ae0370825a812723bf5af
AysenurGunes <gunesaysenur94@gmail.com>
2016-11-08 14:24:17 +0200

    değişiklik

--- KariyerPortali.Admin/Mappings/DomainToViewModelMappingProfile.cs
+++ KariyerPortali.Admin/Mappings/DomainToViewModelMappingProfile.cs
@@ -28,6 +28,7 @@ namespace KariyerPortali.Admin.Mappings
             Mapper.CreateMap<JobApplication, JobApplicationViewModel>();
             Mapper.CreateMap<University, UniversityViewModel>();
             Mapper.CreateMap<Employer, EmployerFormViewModel>();
+            Mapper.CreateMap<Employer, FileFormViewModel>();
 #pragma warning restore CS0618 // Type or member is obsolete
         }
     
