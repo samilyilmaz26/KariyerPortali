b657a9c1fb4f7cb11b5dbd19f4db07f1d658a4f0
ArifeOzdemir <ozdemir.ariffe@gmail.com>
2016-11-08 16:41:13 +0200

    dtgdsh

---
+++ KariyerPortali.Admin/ViewModels/LanguageFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KariyerPortali.Admin.ViewModels
+{
+    public class LanguageFormViewModel
+    {
+        public int LanguageId { get; set; }
+        public string LanguageName { get; set; }
+    }
+}
