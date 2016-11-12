b3c4be0dc8ed98704a8ef97433979ee41c2c3494
ecembaysal <ecem.baysal@live.com>
2016-11-12 14:41:10 +0300

    Career Controller & Özgeçmişim View Oluşturulması

---
+++ KariyerPortali.Web/Controllers/CareerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KariyerPortali.Web.Controllers
+{
+    public class CareerController : Controller
+    {
+        // GET: Career
+        public ActionResult Ozgecmisim()
+        {
+            return View();
+        }
+    }
+}
