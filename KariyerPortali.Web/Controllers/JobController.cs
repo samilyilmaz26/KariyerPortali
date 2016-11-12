e8cd3392bf871c07e582d0a9b6d834b10bd2e80a
ecembaysal <ecem.baysal@live.com>
2016-11-12 13:50:20 +0300

    Job controller & Is Ara Action Eklenmesi

---
+++ KariyerPortali.Web/Controllers/JobController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KariyerPortali.Web.Controllers
+{
+    public class JobController : Controller
+    {
+        // GET: Job
+        public ActionResult IsAra()
+        {
+            return View();
+        }
+    }
+}
