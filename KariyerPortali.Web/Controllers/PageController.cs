c6901ef6ea45d81b36b0fa3599caa7fa27c13617
ecembaysal <ecem.baysal@live.com>
2016-11-12 14:15:32 +0300

    Page Controller & Hakkımızda ve İletisim Viewlarının Oluşturulması

---
+++ KariyerPortali.Web/Controllers/PageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KariyerPortali.Web.Controllers
+{
+    public class PageController : Controller
+    {
+        // GET: Page
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+    }
+}
