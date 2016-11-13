cbab5b6ee8864ff38c97c093d2f92bd3b03009df
ecembaysal <ecem.baysal@live.com>
2016-11-13 12:24:07 +0300

    İlanDetay Actionu ve View'i eklenmesi

--- KariyerPortali.Web/Controllers/JobController.cs
+++ KariyerPortali.Web/Controllers/JobController.cs
@@ -13,5 +13,10 @@ namespace KariyerPortali.Web.Controllers
         {
             return View();
         }
+        public ActionResult IlanDetay()
+        {
+            return View();
+        }
+
     }
 }
